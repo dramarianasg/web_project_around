881d4abc9abe62a83423de9d736422c9ae27674f
dramarianasg <dramarianasg@gmail.com>
2024-10-30 21:55:10 -0600

    correcciones

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Variables Edicion Perfil
+const profileTitle = document.querySelector(
+  ".content__profile_group-infotitle"
+); //Título
+const profileAbout = document.querySelector(
+  ".content__profile_group-infosubtitle"
+); //Subtítulo
 const EditButton = document.querySelector("#EditButton"); // Boton edicion en perfil
 const popup = document.querySelector(".popup"); /* Area popup */
 const popupOverlay =
   document.querySelector(".popup__overlay"); /* Detras del formulario */
-const popupForm =
-  document.querySelector(".popup__form"); /* Formulario pop up */
+const popupContainer =
+  document.querySelector(".popup__container"); /* Formulario pop up */
 const popupOpened =
   document.querySelector(".popup__opened"); /* Formulario abierto */
 const CloseButton =
@@ -17,19 +23,19 @@ const popupSubmit =
   document.querySelector(".popup__submit"); /* Boton enviar info */
 
 // Apertura del popup
-function openPopup(popupForm) {
-  popupForm.classList.add(".popup__opened");
+function openPopup(popupContainer) {
+  popupContainer.classList.add("popup__opened");
 }
 
 // Cierre del popup
-function closePopup(popup) {
-  popup.classList.remove("popup__opened");
+function closePopup(popupContainer) {
+  popupContainer.classList.remove("popup__opened");
 }
 
 // Activacion click para apertura de edicion de perfil
 EditButton.addEventListener("click", function () {
-  inputName.textContent = popupName.value;
-  inputInfo.textContent = popupInfo.value;
+  profileTitle.textContent = popupName.value;
+  profileAbout.textContent = popupInfo.value;
   openPopup(popup);
 });
 
